7ec0d3222410026baa8ef7ded3ae6f7c62a75891
Ekaitz <ekaitz@creative-tweed.com>
2019-10-25 21:22:18 +0100

    geolocating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser')
+const axios = require('axios');
 const { check, validationResult } = require('express-validator');
 
 const app = express();
@@ -13,13 +14,12 @@ app.use(bodyParser.urlencoded({
 
 
 app.post('/address',[
-    // username must be an email
     check('street').exists(),
     check('streetNumber').exists(),
     check('town').exists(),
     check('postalCode').exists(),
     check('country').exists()
-  ], function (req, res) {
+  ], (req, res) => {
     
     console.log('req.body', req.body)
     const { street, streetNumber, town, postalCode, country}  = req.body;
@@ -36,8 +36,32 @@ app.post('/address',[
     })
 });
 
+getLatLng = async (address) =>{
+    const apiKey = 'AIzaSyCQTqvvgVbyyUdLMVIEAcyzMp3YRUlAI7Y';
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
+
+    try { 
+        const { results } = (await axios.get(url)).data;
+        const { location } = results.pop().geometry;
+        return location;
+    } catch (error) {
+        throw Error(error);
+    }
+}
+
+app.get('/weather', async (req, res) =>{
+
+    const { address } = req.query;
+    console.log('here', address)
+    try {
+        const location = await getLatLng(address);
+        res.json(location);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
 
