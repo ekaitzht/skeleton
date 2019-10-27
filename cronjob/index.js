71eefbf850b5fad54d7cf00115575cbd1865cd01
Ekaitz <ekaitz@creative-tweed.com>
2019-10-27 10:30:53 +0000

    Calling each 3 seconds to mongodb and retrieving docs

--- cronjob/index.js
+++ cronjob/index.js
@@ -1,11 +1,23 @@
 const MongoClient = require("mongodb").MongoClient;
 
-console.log("hellllllooooooo");
-/*const uri = "mongodb/wefox?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("addresses");
-  // perform actions on the collection object
-  client.close();
-});
-*/
+console.log("Connecting ");
+const client = new MongoClient("mongodb://mongodb:27017/wefox");
+setTimeout(() => {
+  client.connect(err => {
+    const collection = client
+      .db("wefox")
+      .collection("addresses")
+      .find();
+    // perform actions on the collection object
+    console.log("collectiton", collection);
+  });
+
+  setInterval(() => {
+    const collection = client
+      .db("wefox")
+      .collection("addresses")
+      .find();
+    // perform actions on the collection object
+    console.log("collectiton", collection);
+  }, 3000);
+}, 3000);
