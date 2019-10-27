73e9499788ec84ba4554cc79284d591ce99e7e1a
Ekaitz <ekaitz@creative-tweed.com>
2019-10-27 10:08:38 +0000

    Executing node

---
+++ cronjob/index.js
@@ -0,0 +1,11 @@
+const MongoClient = require("mongodb").MongoClient;
+
+console.log("hellllllooooooo");
+/*const uri = "mongodb/wefox?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("addresses");
+  // perform actions on the collection object
+  client.close();
+});
+*/
